9ae695c373af3f3805c4e101943fc1cb2f42cd08
DhruvinMulani <mulanidhruvin@gmail.com>
2024-02-19 13:14:19 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Dhruvin Mulani on 2024-02-19.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
